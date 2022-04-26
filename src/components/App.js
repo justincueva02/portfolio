18d5813eeb426641379468088505c0f49abc15ee
justincueva02 <justincueva02@gmail.com>
2022-04-26 13:03:19 -0700

    built our footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import LandingPage from "./landingPage/LandingPage";
 import Projects from "./projects/Projects";
+import Footer from "./footer/Footer";
 
 const App = () => {
   return (
     <div>
       <LandingPage />
       <Projects />
+      <Footer />
     </div>
   );
 };
