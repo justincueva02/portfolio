a07ef10e037d480ec07a55b206c022c52f2e54a7
justincueva02 <justincueva02@gmail.com>
2022-04-25 23:48:00 -0700

    built navigation bar

---
+++ src/components/landingPage/LandingPage.js
@@ -0,0 +1,55 @@
+import "../../styles/landingPage.css";
+import { IoIosContrast } from "react-icons/io";
+
+const LandingPage = () => {
+  return (
+    <section id="landing-page">
+      <nav>
+        <figure>
+          {/* <img id="personal-logo" src="" alt="" /> */}
+          {/* will make a personal logo later */}
+          <div id="personal-logo"></div>
+        </figure>
+        <ul className="nav__link--list">
+          <li className="nav__link">
+            <a
+              href="#"
+              className="
+              nav__link--anchor 
+              link__hover-effect 
+              link__hover-effect--black"
+            >
+              About
+            </a>
+          </li>
+          <li className="nav__link">
+            <a
+              href="#"
+              className="
+              nav__link--anchor 
+              link__hover-effect 
+              link__hover-effect--black"
+            >
+              Projects
+            </a>
+          </li>
+          <li className="nav__link">
+            <a
+              href="#"
+              className="
+              nav__link--anchor 
+              link__hover-effect 
+              link__hover-effect--black 
+              icon--contrast
+              "
+            >
+              <IoIosContrast />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default LandingPage;
