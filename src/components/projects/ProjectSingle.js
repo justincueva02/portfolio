d0c540d1eee67bc062862221d87860363cf8e0dc
justincueva02 <justincueva02@gmail.com>
2022-04-26 12:25:51 -0700

    dynamically providing the project data

--- src/components/projects/ProjectSingle.js
+++ src/components/projects/ProjectSingle.js
@@ -1,25 +1,24 @@
-import img from "../../assets/blinckerMockup.png";
-
 import { BsGithub } from "react-icons/bs";
 import { BiLink } from "react-icons/bi";
 import "../../styles/projects/projects.css";
 
-const ProjectSingle = () => {
+const ProjectSingle = ({ projectData }) => {
   return (
     <li className="project">
       <div className="project__wrapper">
-        <img className="project__img" alt="temp project image" src={img} />
+        <img
+          className="project__img"
+          alt="temp project image"
+          src={projectData.image}
+        />
         <div className="project__wrapper--bg"></div>
         <div className="project__description">
-          <h3 className="project__description--title">Car Sales Project</h3>
+          <h3 className="project__description--title">{projectData.title}</h3>
           <h4 className="project__description--sub-title">
-            React, Javascript, TypeScript, CSS, Html
+            {projectData.tech}
           </h4>
           <p className="project__description--para">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat
+            {projectData.description}
           </p>
           <div className="project__description--links">
             <a href="#" className="project__description--link">
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,32 @@
 import ProjectSingle from "./ProjectSingle";
 import "../../styles/projects/projects.css";
 
+import projectImg1 from "../../assets/blinckerMockup.png";
+
+const projectsData = [
+  {
+    title: "Car Sales Project",
+    tech: "React, Javascript, TypeScript, CSS, Html",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image: projectImg1,
+  },
+  {
+    title: "Car Sales Project",
+    tech: "React, Javascript, TypeScript, CSS, Html",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image: projectImg1,
+  },
+  {
+    title: "Car Sales Project",
+    tech: "React, Javascript, TypeScript, CSS, Html",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    image: projectImg1,
+  },
+];
+
 const Projects = () => {
   return (
     <section id="projects">
@@ -10,7 +36,9 @@ const Projects = () => {
             Here are some of my <span className="red">projects</span>
           </h1>
           <ul className="project__list">
-            <ProjectSingle />
+            {projectsData.map((projectData, index) => {
+              return <ProjectSingle projectData={projectData} key={index} />;
+            })}
           </ul>
         </div>
       </div>
