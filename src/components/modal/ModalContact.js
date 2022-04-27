7d39653f326728c04d7ba0fef93eceb62c7e22e7
justincueva02 <justincueva02@gmail.com>
2022-04-26 18:06:46 -0700

    styled loading and success modal states

--- src/components/modal/ModalContact.js
+++ src/components/modal/ModalContact.js
@@ -1,54 +1,71 @@
+import { Fragment } from "react";
+import { ImSpinner6 } from "react-icons/im";
+import { FaTimes } from "react-icons/fa";
+
 import "../../styles/modal/modalContact.css";
 
 const ModalContact = () => {
   return (
-    <div className="modal__half modal__contact">
-      <h3 className="modal__title modal__title--contact">Let's have a chat</h3>
-      <h3 className="modal__sub-title modal__sub-title--contact">
-        I'm currently open to new opportunities
-      </h3>
-      <form id="contact__form">
-        <div className="form__item">
-          <label className="form__item--label" htmlFor="name">
-            Name
-          </label>
-          <input
-            autoComplete="off"
-            className="input"
-            name="user_name"
-            type={"text"}
-            required
-          />
+    <Fragment>
+      <div className="modal__half modal__contact">
+        <h3 className="modal__title modal__title--contact">
+          Let's have a chat
+        </h3>
+        <h3 className="modal__sub-title modal__sub-title--contact">
+          I'm currently open to new opportunities
+        </h3>
+        <form id="contact__form">
+          <div className="form__item">
+            <label className="form__item--label" htmlFor="name">
+              Name
+            </label>
+            <input
+              autoComplete="off"
+              className="input"
+              name="user_name"
+              type={"text"}
+              required
+            />
+          </div>
+          <div className="form__item">
+            <label className="form__item--label" htmlFor="email">
+              Email
+            </label>
+            <input
+              autoComplete="off"
+              className="input"
+              name="user_email"
+              type={"email"}
+              required
+            />
+          </div>
+          <div className="form__item">
+            <label className="form__item--label" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              autoComplete="off"
+              className="input"
+              name="user_message"
+              type={"text"}
+              required
+            />
+          </div>
+          <button id="contact__submit" className="form__submit">
+            Send it my way
+          </button>
+        </form>
+        <div className="modal__overlay modal__overlay--loading">
+          <ImSpinner6 className="spinner" />
         </div>
-        <div className="form__item">
-          <label className="form__item--label" htmlFor="email">
-            Email
-          </label>
-          <input
-            autoComplete="off"
-            className="input"
-            name="user_email"
-            type={"email"}
-            required
-          />
+        <div className="modal__overlay modal__overlay--success">
+          Thanks for the message! Looking forward to speaking to you soon.
         </div>
-        <div className="form__item">
-          <label className="form__item--label" htmlFor="message">
-            Message
-          </label>
-          <textarea
-            autoComplete="off"
-            className="input"
-            name="user_message"
-            type={"text"}
-            required
-          />
-        </div>
-        <button id="contact__submit" className="form__submit">
-          Send it my way
-        </button>
-      </form>
-    </div>
+      </div>
+      <span className="modal__exit click">
+        <FaTimes />
+      </span>
+    </Fragment>
   );
 };
 
