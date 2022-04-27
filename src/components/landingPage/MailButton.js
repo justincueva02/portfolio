932ee3b435dddbda8846efa73de55b001773d001
justincueva02 <justincueva02@gmail.com>
2022-04-27 13:08:49 -0700

    made website responsive

--- src/components/landingPage/MailButton.js
+++ src/components/landingPage/MailButton.js
@@ -10,12 +10,14 @@ const MailButton = () => {
   const modalContext = useContext(ModalContext);
   return (
     <Fragment>
-      <button
-        className="mail__btn click"
-        onClick={() => modalContext.setModalIsOpen((prev) => !prev)}
-      >
-        <GrMail />
-      </button>
+      <a href="#">
+        <button
+          className="mail__btn click"
+          onClick={() => modalContext.setModalIsOpen((prev) => !prev)}
+        >
+          <GrMail />
+        </button>
+      </a>
       <a href="#projects" className="scroll">
         <div className="scroll__icon click"></div>
       </a>
