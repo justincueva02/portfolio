b7de5224d32a6b709274aa848e6a740ff5dda7a4
justincueva02 <justincueva02@gmail.com>
2022-04-27 12:04:07 -0700

    animating the shapes

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -9,8 +9,23 @@ import "../../styles/landingPage/landingPage.css";
 
 const LandingPage = ({ setIsDarkTheme }) => {
   const modalContext = useContext(ModalContext);
+  const moveBackground = (e) => {
+    const shapes = document.querySelectorAll(".shape");
+    const x = e.clientX / 20;
+    const y = e.clientY / 20;
+    console.log(x, y);
+
+    for (let i = 0; i < shapes.length; i++) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${
+        y * boolInt
+      }px)`;
+    }
+  };
+
   return (
-    <section id="landing-page">
+    <section id="landing-page" onMouseMove={(e) => moveBackground(e)}>
       {!modalContext.modalIsOpen && <NavBar setIsDarkTheme={setIsDarkTheme} />}
       {!modalContext.modalIsOpen && <Header />}
       <MailButton />
